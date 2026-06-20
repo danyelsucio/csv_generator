915351230c8a5a9ef66abe8ce8e7e63ba5af87fe
danyelsucio <hdezdan45@gmail.com>
2026-06-19 20:50:47 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'JARVIS Scanner',
+      title: 'Adrianita csv',
       theme: ThemeData.dark(),
       home: const HomePage(),
       debugShowCheckedModeBanner: false,
